5d02ae50e77146c8f2e7248655fed952094ff04e
leeyujin <linoonil@hanmail.net>
2022-09-01 11:33:35 +0900

    add : login.js

--- src/pages/yujinLee/Login/Login.js
+++ src/pages/yujinLee/Login/Login.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import "./Login.scss";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <h1>Login입니다.</h1>;
+  // const navigate = useNavigate();
+
+  // const goToMain = () => {
+  //   navigate("./Main");
+  // };
+
+  return (
+    <div className="wrapper">
+      <h1 className="logo">Westagram</h1>
+        <form className="inputContainer">
+            <div>
+                <input type="text" maxlength="20" className="login-id" placeholder="전화번호, 사용자 이름 또는 이메일"/>
+                <input type="password" maxlength="11" className="login-pw" placeholder="비밀번호"/>
+                <button type="submit" className="login-btn" name="login">로그인</button>
+            </div>
+            <a href="">
+                <div className="forgotPw" >비밀번호를 잊으셨나요?</div>
+            </a>
+        </form>
+    </div>
+  );
 };
 
 export default Login;
