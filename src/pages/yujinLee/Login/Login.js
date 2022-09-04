276930a06518f8edd82ab0ec7f3b8e3dfb7b868b
leeyujin <linoonil@hanmail.net>
2022-09-04 10:57:47 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/yujinLee/Login/Login.js
+++ src/pages/yujinLee/Login/Login.js
@@ -1,26 +1,66 @@
-import "./Login.scss";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import './Login.scss';
 
 const Login = () => {
-  // const navigate = useNavigate();
+  const [userId, setUserId] = useState('전화번호, 사용자 이름 또는 이메일');
+  const [userPw, setUserPw] = useState('비밀번호');
 
-  // const goToMain = () => {
-  //   navigate("./Main");
-  // };
+  const saveUserId = e => {
+    setUserId(e.target.value);
+  };
+  console.log(userId);
+
+  const saveUserPw = e => {
+    setUserPw(e.target.value);
+  };
 
   return (
     <div className="wrapper">
-      <h1 className="logo">Westagram</h1>
-        <form className="inputContainer">
-            <div>
-                <input type="text" maxlength="20" className="login-id" placeholder="전화번호, 사용자 이름 또는 이메일"/>
-                <input type="password" maxlength="11" className="login-pw" placeholder="비밀번호"/>
-                <button type="submit" className="login-btn" name="login">로그인</button>
-            </div>
-            <a href="">
-                <div className="forgotPw" >비밀번호를 잊으셨나요?</div>
-            </a>
+      <div className="loginBox">
+        <div className="logo">westagram</div>
+        <form className="Container">
+          <input
+            onChange={saveUserId}
+            type="text"
+            className="loginId"
+            name="userID"
+            minLength="5"
+            maxLength="20"
+            autoComplete="off"
+            autoFocus="turu"
+            placeholder={userId}
+          />
+          <input
+            onChange={saveUserPw}
+            type="password"
+            className="loginPw"
+            name="userPW"
+            maxLength="11"
+            autoComplete="off"
+            placeholder={userPw}
+          />
+          <button
+            // onClick={''}
+            type="submit"
+            className="loginBtn"
+            // disabled="disabled"
+            name="login"
+          >
+            로그인
+          </button>
         </form>
+        <a href="#!">
+          <div className="forgotPw">비밀번호를 잊으셨나요?</div>
+        </a>
+      </div>
+      <div className="createAcount">
+        <p>
+          계정이 없으신가요?
+          <a href="#!">
+            <span>가입하기</span>
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
