d84fb82d1636238bc62499a9cf5fe22d0a14cfc1
ahn-sujin <asj9674@gmail.com>
2022-09-07 13:35:48 +0900

    Modify: 피드 별로 댓글 state 따로 관리 되도록 수정

--- src/pages/sujinAhn/Main/Contents/Contents.js
+++ src/pages/sujinAhn/Main/Contents/Contents.js
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
 import Feeds from "./Components/Feeds";
 import Etc from "./Components/Etc";
 
 const Contents = () => {
+  const [feeds, setFeeds] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/sujinAhn/feedInfoList.json")
+      .then(res => res.json())
+      .then(feedInfo => setFeeds(feedInfo));
+  }, []);
+
   return (
     <main className="container">
-      <Feeds />
+      <div className="feeds">
+        {feeds.map(feedList => (
+          <Feeds key={feedList.id} feedList={feedList} />
+        ))}
+      </div>
       <Etc />
     </main>
   );
